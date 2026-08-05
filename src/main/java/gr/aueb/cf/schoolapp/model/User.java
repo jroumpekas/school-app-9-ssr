0c5c8f19e1989df89e6de11a431cbd363f80514e
Dim.Roumpekas <jimroumpi@gmail.com>
2026-08-05 12:50:22 +0300

    fix: incorrect Role class import

--- src/main/java/gr/aueb/cf/schoolapp/model/User.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/User.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import gr.aueb.cf.schoolapp.model.Role;
+
 import java.util.*;
 
 @Entity
